f5f8da17869771ea4cac508f23db7643d2c3bb36
Ricardo arango <ricardoarango@MBP-de-Ricardo.lan>
2015-09-09 10:29:13 -0500

    Initial Commit

---
+++ Triqui/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Triqui
+//
+//  Created by Ricardo arango on 9/09/15.
+//  Copyright (c) 2015 Ideas App Colombia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
